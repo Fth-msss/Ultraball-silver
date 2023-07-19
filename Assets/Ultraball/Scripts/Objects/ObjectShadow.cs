70cc914f79001dca7523e00db6378807e41697cb
Fth-msss <fatih.donmez2008@hotmail.com>
2023-07-19 04:07:19 +0300

    ObjectShadow Script now works with any collider

--- Assets/Ultraball/Scripts/Objects/ObjectShadow.cs
+++ Assets/Ultraball/Scripts/Objects/ObjectShadow.cs
@@ -2,37 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 /// <summary>
 /// this script handles ground checks and drop shadows.
-/// man this needs a redo
 /// </summary>
 public class ObjectShadow : MonoBehaviour
 {
-    //layermask is just standable
+    
     [SerializeField] LayerMask standablelayermask;
 
     //shadow of object
     public GameObject shadow;
 
+    public GameObject OriginObject;
 
-    //variables
-    [SerializeField]
-    SphereCollider objectCollider;
+    Collider objectCollider;
+
+
 
     [SerializeField]
     [Tooltip("length of boxcast")]
     float maxHeightCheck = 500f;
 
     [SerializeField]
-    [Tooltip("for debug")]
+    [Tooltip("public for debug")]
     public bool isGrounded = false;
 
     [SerializeField]
     [Tooltip("how close should object be to a surface to count as grounded")]
     float groundcheckrange = 0.2f;
-
-    public event EventHandler<GroundedEventArgs> grounded;
 
     [Header("shadow settings")]
     [SerializeField]
@@ -44,21 +43,27 @@ public class ObjectShadow : MonoBehaviour
     [SerializeField]
     float minimumDistance = 1f;
 
+    public event EventHandler<GroundedEventArgs> groundedEvent;
+
     private void Start()
     {
-        if (objectCollider == null) { objectCollider = GetComponent<SphereCollider>(); }
+       
+        if (objectCollider == null) { objectCollider = GetComponent<Collider>(); }
 
+        if (OriginObject == null) 
+        {
+            OriginObject= GameObject.Find("OriginObject");
+        }
+
+        ConstraintSource constraintSource = new ConstraintSource();
+        constraintSource.sourceTransform = OriginObject.transform;
+        constraintSource.weight = 1;
+        shadow.GetComponent<RotationConstraint>().AddSource(constraintSource);
 
     }
 
     void Update()
     {
-
-
-
-
-
-
         bool isHit = Physics.BoxCast(objectCollider.bounds.center, objectCollider.bounds.extents - new Vector3(0.06f, 0.06f, 0.1f), new Vector3(0, -1, 0), out RaycastHit hit, Quaternion.identity, maxHeightCheck, standablelayermask);
 
 
@@ -75,7 +80,7 @@ public class ObjectShadow : MonoBehaviour
                 if (!isGrounded) //if it was not grounded before
                 {
                     isGrounded = true;
-                    grounded?.Invoke(this, new GroundedEventArgs { IsGrounded = isGrounded });
+                    groundedEvent?.Invoke(this, new GroundedEventArgs { IsGrounded = isGrounded });
                 }
 
             }
@@ -84,7 +89,7 @@ public class ObjectShadow : MonoBehaviour
                 if (isGrounded) //if it was grounded before
                 {
                     isGrounded = false;
-                    grounded?.Invoke(this, new GroundedEventArgs { IsGrounded = isGrounded });
+                    groundedEvent?.Invoke(this, new GroundedEventArgs { IsGrounded = isGrounded });
                 }
                 isGrounded = false;
             }
@@ -95,7 +100,7 @@ public class ObjectShadow : MonoBehaviour
             if (isGrounded) //if grounded before
             {
                 isGrounded = false;
-                grounded?.Invoke(this, new GroundedEventArgs { IsGrounded = isGrounded });
+                groundedEvent?.Invoke(this, new GroundedEventArgs { IsGrounded = isGrounded });
             }
             //disable shadow when on void
             shadow.SetActive(false);
@@ -119,7 +124,7 @@ public class ObjectShadow : MonoBehaviour
 
         float shadowscale = 1;
 
-        //determine shadow scale depending on distance 
+        //calculate shadow scale depending on distance 
         if (distance <= minimumDistance)
         {
             shadowscale = 1;
@@ -145,84 +150,6 @@ public class ObjectShadow : MonoBehaviour
     }
 
 
-
-    //gizmos
-    /*
-    private void OnDrawGizmos()
-    {
-        Vector3 shadowpoint;
-        RaycastHit hit;
-
-        SphereCollider collider = GetComponent<SphereCollider>();
-        bool isHit =Physics.BoxCast(collider.bounds.center, collider.bounds.extents - new Vector3(0.06f, 0.06f, 0.1f), new Vector3(0,-1,0), out RaycastHit raycastHit, Quaternion.identity, 1, standablelayermask);
-
-
-        if (isHit) 
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireCube(transform.position + new Vector3(0, -1, 0) * raycastHit.distance, transform.lossyScale);
-        }
-        else 
-        {
-            Gizmos.color = Color.yellow;
-         
-        }
-
-
-        RaycastHit hit2;
-        float maxheigthcerck=500f;
-
-        bool isHit2 = Physics.Raycast(collider.bounds.center, new Vector3(0.00f, -1f, 0f),out RaycastHit hithit,maxheigthcerck);
-      
-        if (isHit2) 
-        {
-            Gizmos.DrawRay(transform.position, new Vector3(0.00f, -1f, 0f) * hithit.distance);
-            //Debug.Log(hithit.point);
-            shadowpoint = hithit.point;
-        }
-        else 
-        {
-            
-        }
-
-        float shadowmaxsize = 1;
-        float shadowminsize = 0.5f;
-        //golgenin maksimum ve minimum degerlerini alacagi distance degerlerini sec
-        //maksimum = distance<1
-        //minimum = distance>10
-        float maximumdistance = 10;
-
-      
-        float currentshadowsize = 1;
-
-        //get shadow size. shadow gets smaller when further from ground
-        if (hithit.distance <= 1) 
-        {
-            currentshadowsize = 1;
-            //Debug.Log("test:" + currentshadowsize);
-        }
-        else if (hithit.distance >= 10) 
-        {
-            currentshadowsize = 0.5f;
-            //Debug.Log("test:" + currentshadowsize);
-        }
-        else 
-        {
-
-            float percentage = (hithit.distance-1) / (10-1) * 100f;
-            currentshadowsize =  1f-0.5f / 100* percentage;
-           // Debug.Log("test:" + currentshadowsize);
-
-
-
-        }
-        
-
-        //set shadow position and size(on an actual method) 
-
-
-    }
-    */
 }
 
 public class GroundedEventArgs : EventArgs
